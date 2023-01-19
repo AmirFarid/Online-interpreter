6310ac72fd79a8cd9624cd33c4cac37f9aaa4f6a
Amir Farid <52009933+AmirFarid@users.noreply.github.com>
2023-01-20 01:25:48 +0330

    initiate Test phase

--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
+import unittest
 from flask.cli import FlaskGroup
 from project import app, db
 
 cli = FlaskGroup(app)
 
+@cli.command()
+def test():
+	""" Runs the tests without code coverage"""
+	tests = unittest.TestLoader().discover('project/test', pattern='test*.py')
+	result = unittest.TextTestRunner(verbosity=2).run(tests)
+	if result.wasSuccessful():
+		return 0
+	return 1
 
 @cli.command()
 def recreatedb():
